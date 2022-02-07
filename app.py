0309e07edcb16cb6a923edcdda89960917af0a0c
Varun11101 <90500051+Varun11101@users.noreply.github.com>
2022-02-07 16:38:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,17 +3,32 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:root123@server/dbms_mini"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root123@localhost/dbms_mini"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    users = db.engine.execute('Select * from user')
+    users_list = [ row for row in users]
+    print(users_list)
+    return render_template('index.html',  users_list=users_list)
 
 @app.route("/users")
 def users():
     return render_template('users.html')
+
+@app.route("/blogs")
+def blogs():
+    return render_template('blogs.html')
+
+@app.route("/contests")
+def contests():
+    return render_template('contests.html')
+
+@app.route("/problems")
+def problems():
+    return render_template('problems.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
