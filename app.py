fbae647177fc35953626e5047762ef9ee2f4b09c
Varun11101 <90500051+Varun11101@users.noreply.github.com>
2022-02-07 20:37:19 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from flask import *
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root123@localhost/dbms_mini"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root123@localhost/db_mini"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "xxx"
 db = SQLAlchemy(app)
 
 @app.route("/")
@@ -18,49 +19,55 @@ def hello_world():
 
 @app.route('/users')
 def users():
-	users = db.engine.execute('select * from user')
-	users_list = [row for row in users]
-	return render_template('users.html' , users_list = users_list)
+    users = db.engine.execute('select * from user')
+    users_list = [row for row in users]
+    return render_template('users.html' , users_list = users_list)
 
 @app.route('/add_user' , methods=['GET' , 'POST'])
 def add_user():
-	name = request.form.get('name')
-	country = request.form.get('country')
-	try:
-		db.engine.execute(f'insert into user values {name} ,{country} ')
-	except Exception:
-		db.session.rollback()
-		flash("Some error occured" , "danger" )
-        return redirect(url_for('users'))
-	flash('user added')
-	return redirect(url_for('users'))
-
+   name = request.form.get('name')
+   country = request.form.get('country')
+   id = request.form.get('id')
+   email = request.form.get('email')
+   try:
+    db.engine.execute(f"insert into user values ('{name}' ,'{country}', {id}, '{email}' ) ")
+   except Exception as e:
+    print(e)
+    db.session.rollback()
+    flash("Some error occured" , "danger" )
+    return redirect(url_for('users'))
+   flash('user added')
+   return redirect(url_for('users'))
 
 @app.route('/edit_user/<int:idx>'  , methods=['GET' , 'POST'])
 def edit_user(idx):
-	name = request.form.get('name')
-	country = request.form.get('country')
-	try:
-		db.engine.execute(f'update user set name = {name} , country = {country} where id = {idx} ')
-	except Exception:
-		db.session.rollback()
-		flash("Some error occured" , "danger" )
+    name = request.form.get('name')
+    country = request.form.get('country')
+    id = request.form.get('id')
+    email = request.form.get('email')
+    
+    try:
+        db.engine.execute(f'update user set name = {name} , country = {country}, ID = {id}, GMAIL = {email} where id = {idx} ')
+    except Exception:
+        db.session.rollback()
+        flash("Some error occured" , "danger")
         return redirect(url_for('users'))
-	flash('updated user')
-	return redirect(url_for('users'))
+    flash('updated user')
+    return redirect(url_for('users'))
+
 
 
 @app.route('/delete_user/<int:idx>' , methods=['GET' , 'POST'])
 def delete_user(idx):
-	try:
-		db.engine.execute(f'delete from user where id = {idx}')
-	except Exception:
-		db.session.rollback()
-		flash("Some error occured" , "danger" )
+    try:
+        db.engine.execute(f'delete from user where id = {idx}')
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+        flash("Some error occured" , "danger")
         return redirect(url_for('users'))
-
-	flash('deleted user')
-	return redirect(url_for('users'))
+    flash('Deleted user')
+    return redirect(url_for('users'))
 
 
 @app.route("/blogs")
